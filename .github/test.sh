3aed5ad9e41e9642625969fa8c1da00d68665f02
Oliver Ullman <oliverullman@gmail.com>
2022-07-22 12:00:59 -0600

    adjus

--- .github/test.sh
+++ .github/test.sh
@@ -1,4 +1,4 @@
-git log
+git log -n 5
 git diff --diff-filter=ACMRD --name-only 7c73e5aed106123ebc3587f83134238f0ac7a664..8c388a85b59994709c6b2f5214fc42f16224ec12
 
 branch=$(git rev-parse --abbrev-ref HEAD)
