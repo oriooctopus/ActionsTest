2688037b5fb83469e7c0caa96589029f96ec0a46
Oliver Ullman <oliverullman@gmail.com>
2022-07-22 11:55:09 -0600

    adjus

--- .github/test.sh
+++ .github/test.sh
@@ -1,3 +1,5 @@
+git diff --diff-filter=ACMRD --name-only 7c73e5aed106123ebc3587f83134238f0ac7a664..8c388a85b59994709c6b2f5214fc42f16224ec12
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 mainbranch=$(git remote show origin | grep HEAD | sed -e "s/^.*: //")
 if [ "$branch" == "$mainbranch" ] && [ "$GITHUB_EVENT_BEFORE" ] ; then
