8c388a85b59994709c6b2f5214fc42f16224ec12
Oliver Ullman <oliverullman@gmail.com>
2022-07-22 11:53:20 -0600

    adjus

--- .github/test.sh
+++ .github/test.sh
@@ -1,1 +1,7 @@
-echo "12345"
+branch=$(git rev-parse --abbrev-ref HEAD)
+mainbranch=$(git remote show origin | grep HEAD | sed -e "s/^.*: //")
+if [ "$branch" == "$mainbranch" ] && [ "$GITHUB_EVENT_BEFORE" ] ; then
+  git diff --diff-filter=ACMRD --name-only ${GITHUB_EVENT_BEFORE}..${GITHUB_EVENT_AFTER}
+else
+  git diff --name-only origin/${mainbranch}...
+fi
