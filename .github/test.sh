f1ebd4556395d62491466cf564d02f3ac6b04b15
Oliver Ullman <oliverullman@gmail.com>
2022-08-11 09:49:40 -0300

    working

--- .github/test.sh
+++ .github/test.sh
@@ -1,6 +1,7 @@
 # git diff --diff-filter=ACMRD --name-only 7c73e5aed106123ebc3587f83134238f0ac7a664..8c388a85b59994709c6b2f5214fc42f16224ec12
 
 # branch=$(git rev-parse --abbrev-ref HEAD)
+
 # mainbranch=$(git remote show origin | grep HEAD | sed -e "s/^.*: //")
 
 changedFiles=$(git diff --diff-filter=ACMRD --name-only ${GITHUB_EVENT_BEFORE}..${GITHUB_EVENT_AFTER})
