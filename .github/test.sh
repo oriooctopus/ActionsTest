c0dfa3733aa7fd50bf14da7fe39609b41b5907f4
Oliver Ullman <oliverullman@gmail.com>
2022-08-11 09:55:14 -0300

    working

--- .github/test.sh
+++ .github/test.sh
@@ -1,16 +1,36 @@
+typeofvar () {
+
+    local type_signature=$(declare -p "$1" 2>/dev/null)
+
+    if [[ "$type_signature" =~ "declare --" ]]; then
+        printf "string"
+    elif [[ "$type_signature" =~ "declare -a" ]]; then
+        printf "array"
+    elif [[ "$type_signature" =~ "declare -A" ]]; then
+        printf "map"
+    else
+        printf "none"
+    fi
+
+}
+
 # git diff --diff-filter=ACMRD --name-only 7c73e5aed106123ebc3587f83134238f0ac7a664..8c388a85b59994709c6b2f5214fc42f16224ec12
 
 # branch=$(git rev-parse --abbrev-ref HEAD)
 
 # mainbranch=$(git remote show origin | grep HEAD | sed -e "s/^.*: //")
 
-# changedFiles=$(git diff --diff-filter=ACMRD --name-only ${GITHUB_EVENT_BEFORE}..${GITHUB_EVENT_AFTER})
+changedFiles=$(git diff --diff-filter=ACMRD --name-only ${GITHUB_EVENT_BEFORE}..${GITHUB_EVENT_AFTER})
+
+echo 'the type before'
+echo typeofvar changedFiles
+echo 'the type after'
 
 # echo 'before'
 # echo $changedFiles
 # echo 'after'
 
-changedFiles='.github/test.sh test.txt'
+# changedFiles='.github/test.sh test.txt'
 
 IFS=', ' read -r -a array <<< "$changedFiles"
 
